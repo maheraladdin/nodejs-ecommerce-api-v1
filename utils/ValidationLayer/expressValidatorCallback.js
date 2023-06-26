e49f4c417c4252c1a7874c119e32d1d36e1c7fbd
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-06-26 10:13:31 +0300

    UPD expressValidatorCallback.js

--- utils/ValidationLayer/expressValidatorCallback.js
+++ utils/ValidationLayer/expressValidatorCallback.js
@@ -1,8 +1,10 @@
 const handleValidationErrorsMW = require("../../middlewares/handleValidationErrorsMW");
-
-// @desc: express validator refactoring just send rules and middlewares and get the job done
-// @usage: expressValidatorCallback([Rule_1_Name,Rule_2_Name],[middleware_1,middleware_2])
-// @note: you have to send rules and middlewares inside array, and you can send any number of them
+/**
+    @desc: express validator refactoring just send rules and middlewares and get the job done
+    @example: expressValidatorCallback([Rule_1_Name,Rule_2_Name],[middleware_1,middleware_2])
+    @note: you have to send rules and middlewares inside array, and you can send any number of them
+    @param {Array: rules} rules - array of rules
+ */
 module.exports = (rules) => [
     ...rules,
     handleValidationErrorsMW
