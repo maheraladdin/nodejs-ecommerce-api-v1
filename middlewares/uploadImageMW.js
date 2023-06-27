1a0d74a4618dc474f2ce8bfbe72f818aef51c90c
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-06-27 08:51:01 +0300

    UPD uploadImageMW.js

--- middlewares/uploadImageMW.js
+++ middlewares/uploadImageMW.js
@@ -1,45 +1,6 @@
+// Purpose: Multer middleware for uploading one image
 const RequestError = require("../utils/RequestError");
-// const { v4: uuidv4 } = require('uuid');
 const multer = require("multer");
-
-// storage configuration
-// 1) multer Disk Storage
-// use if you want to store files without any image processing
-
-/**
- * @desc    Destination for multer
- * @param   {object} req - request object
- * @param   {object} file - uploaded file object
- * @param   {function} cb - callback function
- * @type    {function}
- */
-// const destination = (req, file, cb) => {
-//     const path = "uploads/categories";
-//     cb(null, path);
-// }
-
-
-/**
- * @desc    Filename for multer
- * @param   {object} req - request object
- * @param   {object} file - uploaded file object
- * @param   {function} cb - callback function
- * @type    {function}
- */
-// const filename = (req, file, cb) => {
-//     const fileExtension = file.mimetype.split("/")[1];
-//     const fileName = `category-${uuidv4()}-${Date.now()}.${fileExtension}`;
-//     cb(null, fileName);
-// }
-
-/**
- * @desc    Multer Disk storage configuration
- * @type    {object}}
- */
-// const storage = multer.diskStorage({destination, filename});
-
-// 2) multer Memory Storage
-// use if you want to store files in memory after apply image processing on them
 
 /**
  * @desc    Multer Memory Storage
