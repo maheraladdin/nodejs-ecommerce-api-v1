2a68dbcdd758d98d45ce6f0ea4ef8c4279fc2c55
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-08-22 22:21:53 +0300

    UPD wishlistController.js

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -37,7 +37,7 @@ module.exports.getUserWishlist = asyncHandler(getUserWishlistHandler);
  *
  */
 const addProductToWishlistHandler = async (req, res) => {
-    const {wishlist} = await User.findByIdAndUpdate(req.user.id, {$addToSet: {wishlist: req.body.product}}, {new: true});
+    const {wishlist} = await User.findByIdAndUpdate(req.user.id, {$addToSet: {wishlist: req.body.product}}, {new: true}).populate('wishlist');
     res.status(200).json({
         status: 'success',
         message: 'Product added to wishlist successfully',
@@ -62,7 +62,7 @@ module.exports.addProductToWishlist = asyncHandler(addProductToWishlistHandler);
  * @param   {object} res - response object
  */
 const removeProductFromWishlistHandler = async (req, res) => {
-    const {wishlist} = await User.findByIdAndUpdate(req.user.id, {$pull: {wishlist: req.params.product}}, {new: true});
+    const {wishlist} = await User.findByIdAndUpdate(req.user.id, {$pull: {wishlist: req.params.product}}, {new: true}).populate('wishlist');
     res.status(200).json({
         message: 'Product removed from wishlist successfully',
         status: 'success',
